4c404466ca548b87626dbc37441718c0e1714cf6
Aekthana <okkratoo@hotmail.com>
2023-03-07 18:43:39 +0700

    'start-pj'

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+import "./App.css";
+function App() {
+    
+  
+  return (
+    <div className="App">
+      <div className="container-image">
+        <img src="/img/anime-1.jpg"  alt="" />
+        <div className="box-text">
+          <h1>Lorem, ipsum dolor.</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit id
+            nemo quidem dolor non corporis expedita harum autem vel? Ea nam
+            repellat saepe repudiandae inventore harum tempore quis quam?
+            Blanditiis rem deserunt cupiditate magnam vel laborum perspiciatis
+            ab facilis dicta. Repellendus, magni! Porro, consequuntur inventore
+            laborum repellendus quidem eveniet? Quaerat totam tempora quia dolor
+            enim repellendus eius a corrupti, laboriosam fugiat, assumenda
+            soluta qui eum, laudantium reiciendis non expedita quibusdam
+            temporibus ipsam aperiam adipisci! Nesciunt eveniet laudantium
+            debitis maiores ipsum asperiores consectetur quibusdam at facilis?
+            Minus dignissimos itaque optio quod, blanditiis eveniet quaerat
+            perferendis provident maxime cumque ab labore veritatis.
+          </p>
+        </div>
+      </div>
+      <div className="container-image">
+        <img src="/img/anime-2.jpg"  alt="" />
+        <div className="box-text">
+          <h1>Lorem, ipsum dolor.</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit id
+            nemo quidem dolor non corporis expedita harum autem vel? Ea nam
+            repellat saepe repudiandae inventore harum tempore quis quam?
+            Blanditiis rem deserunt cupiditate magnam vel laborum perspiciatis
+            ab facilis dicta. Repellendus, magni! Porro, consequuntur inventore
+            laborum repellendus quidem eveniet? Quaerat totam tempora quia dolor
+            enim repellendus eius a corrupti, laboriosam fugiat, assumenda
+            soluta qui eum, laudantium reiciendis non expedita quibusdam
+            temporibus ipsam aperiam adipisci! Nesciunt eveniet laudantium
+            debitis maiores ipsum asperiores consectetur quibusdam at facilis?
+            Minus dignissimos itaque optio quod, blanditiis eveniet quaerat
+            perferendis provident maxime cumque ab labore veritatis.
+          </p>
+        </div>
+      </div>
+      <div className="container-image">
+        <img src="/img/anime-3.jpg"  alt="" />
+        <div className="box-text">
+          <h1>Lorem, ipsum dolor.</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit id
+            nemo quidem dolor non corporis expedita harum autem vel? Ea nam
+            repellat saepe repudiandae inventore harum tempore quis quam?
+            Blanditiis rem deserunt cupiditate magnam vel laborum perspiciatis
+            ab facilis dicta. Repellendus, magni! Porro, consequuntur inventore
+            laborum repellendus quidem eveniet? Quaerat totam tempora quia dolor
+            enim repellendus eius a corrupti, laboriosam fugiat, assumenda
+            soluta qui eum, laudantium reiciendis non expedita quibusdam
+            temporibus ipsam aperiam adipisci! Nesciunt eveniet laudantium
+            debitis maiores ipsum asperiores consectetur quibusdam at facilis?
+            Minus dignissimos itaque optio quod, blanditiis eveniet quaerat
+            perferendis provident maxime cumque ab labore veritatis.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
